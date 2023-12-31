9f33402687379a1f8e1731bfee04c3166e26fa60
Rallenson <Rallenson900@gmail.com>
2023-12-31 01:35:46 -0300

    Add sound effect to game

--- wwwroot/js/Game.js
+++ wwwroot/js/Game.js
@@ -2,7 +2,10 @@ import Configuration from "./Config.js";
 import Player from "./Player.js";
 document.addEventListener("DOMContentLoaded",function() {
     const config = new Configuration();
-    console.log(config)
+    const Sound_hit = new Audio();
+    Sound_hit.src = "./public/Sounds/spaceInavders.wav"
+    console.log(config,Sound_hit)
+    Sound_hit.play();
     function Start(){
         config.currTelas.Drawing();
         config.currTelas.Atualiza();
